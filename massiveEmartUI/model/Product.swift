fc0944c724f184253de5919ea5aac77c2f62242a
salvador <salvador.huerta@neoris.com>
2023-07-24 18:29:19 -0600

    se agregan productos al array

--- massiveEmartUI/model/Product.swift
+++ massiveEmartUI/model/Product.swift
@@ -19,5 +19,14 @@ struct Product : Hashable{
 let misProdductos = [
         Product(title: "Llamarada Moe", image: "ProductLlamaradaMoe", price: 2.00, description: "la mejor bebida nocturna"),
         Product(title: "Rosquilla de Homero", image: "ProductRosquilla", price: 3.00, description:"siper rosquilla de Homero"),
-        Product(title: "Krusty Burguer", image: "ProductBurguer", price: 4.00, description: "la deliciosa hamburguesa Kruzty")
+        Product(title: "Krusty Burguer", image: "ProductBurguer", price: 4.00, description: "la deliciosa hamburguesa Kruzty"),
+        Product(title: "Cerveza Fudd", image: "ProdcutCervezaFudd", price: 4.00, description: "Cerveza Fudd"),
+        Product(title: "Buzz", image: "ProductBuzz", price: 4.00, description: "Buzz"),
+        Product(title: "Cangrejo", image: "ProductCangrejo", price: 4.00, description: "Cangrejo"),
+        Product(title: "Cereal Crusty", image: "ProductCerealCrusty", price: 4.00, description: "Cereal Crusty"),
+        Product(title: "Cerveza Dufff", image: "ProductCervezaDufff", price: 4.00, description: "CervezaDufff"),
+        Product(title: "Lache", image: "ProductLache", price: 4.00, description: "Lache"),
+        Product(title: "Squishe", image: "ProductSquishe", price: 4.00, description: "Squishe"),
+        Product(title: "Tomaco", image: "ProductTomaco", price: 4.00, description: "Tomaco"),
+        Product(title: "Pez Globo", image: "ProductPezGlobo", price: 4.00, description: "Pez Globo")
     ]
